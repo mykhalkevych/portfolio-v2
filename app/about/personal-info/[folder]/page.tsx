c8bf59753107560b691a775a8f3a239dcf249797
Nazar <nazik542@gmail.com>
2025-02-06 12:40:44 +0200

    update config

--- app/about/personal-info/[folder]/page.tsx
+++ app/about/personal-info/[folder]/page.tsx
@@ -3,6 +3,21 @@
 import { CodeBlock, dracula } from 'react-code-blocks';
 import { aboutData } from '../../data';
 import TypeIt from 'typeit-react';
+
+export async function generateStaticParams() {
+  // Fetch or generate a list of dynamic params (e.g., slugs or ids)
+  const slugs = await fetchSlugs(); // Assume fetchSlugs() fetches your slugs
+
+  // Return an array of params for each dynamic page to be pre-rendered
+  return slugs.map((slug) => ({
+    params: { folder: slug },
+  }));
+}
+
+async function fetchSlugs() {
+  return Object.keys(aboutData);
+}
+
 export default function Page({ params }: { params: { folder: string } }) {
   const text: string = aboutData[params.folder];
 
